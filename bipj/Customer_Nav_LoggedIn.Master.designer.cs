da9d848e4df905eae8adaf1e92b58918daa463ae
yjcha <yjchang11062006@gmail.com>
2025-06-25 17:21:47 +0800

    Revert "education1 commit"
    This reverts commit 276ae8a0e7a9e6d48d3592364f01cf3826765b4b.

--- bipj/Customer_Nav_LoggedIn.Master.designer.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace bipj
-{
-    public partial class Customer_Nav_LoggedIn
-    {
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
-        protected global::System.Web.UI.HtmlControls.HtmlForm form2;
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
-    }
-}

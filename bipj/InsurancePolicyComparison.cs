a264cc1c9c98d760f5a0f4803b75521a8efffa44
InSwift <125145281+InSwiftJH@users.noreply.github.com>
2025-08-13 19:01:06 +0800

    added two cs files

---
+++ bipj/InsurancePolicyComparison.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+
+namespace bipj
+{
+    /// <summary>
+    /// Represents the details of a single insurance policy for comparison.
+    /// </summary>
+    public class InsurancePolicyComparison
+    {
+        [JsonProperty("feature")]
+        public string Feature { get; set; }
+
+        [JsonProperty("integratedShield")]
+        public string IntegratedShield { get; set; }
+
+        [JsonProperty("termLife")]
+        public string TermLife { get; set; }
+
+        [JsonProperty("criticalIllness")]
+        public string CriticalIllness { get; set; }
+    }
+}
